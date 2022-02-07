990e8cb024450f2c2273083e6136a7d99c9d3743
NOTORY <97148865+NO-TORY@users.noreply.github.com>
2022-02-07 16:36:23 +0900

    Update this.py

--- VocaroLyrics/this.py
+++ VocaroLyrics/this.py
@@ -1,11 +1,11 @@
-import requests, re, html
+import requests, regex, html
 
 from typing import Optional, Any
 from .exc import SongNotFound
 
 def search(*song: Any, artist: Optional[str] = ""):
     response = requests.get("https://www.google.com/search?q={}+site%3Avocaro.wikidot.com".format(str(*song) + artist))
-    s = re.findall(r"http://vocaro.wikidot.com/.*", html.unescape(response.text))
+    s = regex.findall(r"http://vocaro.wikidot.com/.*", html.unescape(response.text))
     try:
         d = s[0][0:s[0].index("&")]
     except:
@@ -22,15 +22,15 @@ def get_lyrics(*song: Any, indents: Optional[int] = 1, artist: Optional[str] = "
     lyrics = requests.get(song_url).text
     lyrics = html.unescape(lyrics)
 
-    d = re.findall(r"<td>.*", lyrics)
+    d = regex.findall(r"<td>.*", lyrics)
     s = str("\n"*indents).join(d)
-    s = re.sub(r"(<td>)|(</td>)", "", s)
+    s = regex.sub(r"(<td>)|(</td>)", "", s)
     s = s.replace("<span class=\"rt\">", "(")
     s = s.replace("</span></span>", ")")
     s = s.replace("<span class=\"ruby\">", "")
-    s = re.sub(r"(<a class.*)|(<a href.*)", "", s)
+    s = regex.sub(r"(<a class.*)|(<a href.*)", "", s)
     return {
-        "vocaloid": re.sub(r"(<a href=\".*\">)|(</a>)|(<td>)|(</td>)", "", d[3]),
-        "original_url": re.sub(r"(target.*)", "", d[0].split("href=")[1].replace("\"", "")),
+        "vocaloid": regex.sub(r"(<a href=\".*\">)|(</a>)|(<td>)|(</td>)", "", d[3]),
+        "original_url": regex.sub(r"(target.*)", "", d[0].split("href=")[1].replace("\"", "")),
         "lyrics": s.lstrip().rstrip()
     }
