82a04490ebbb28c9f7e14746e5d4d2ff7eea5822
NOTORY <97148865+NO-TORY@users.noreply.github.com>
2022-02-07 22:35:22 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,17 @@ try:
 except:
     from distutils.core import setup
 
+from io import open
+
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="VocaroLyrics",
     version="1.0.3",
     description="보카로 가사 위키에서 가사를 검색해주는 라이브러리",
     author="노토리",
-    install_requires=["requests", "regex", "future-annotations; python_version < 3"],
+    install_requires=requirements,
     py_modules=["VocaroLyrics"],
     long_description="[보카로 가사 위키](http://vocaro.wikidot.com/)",
     url="https://github.com/NO-TORY/VocaroLyricsSearcher",
