799e1281e34d2d7914f2029d0e21c6f11d4962cb
와장창하고나타남 <97148865+NO-TORY@users.noreply.github.com>
2022-02-10 22:13:44 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+from setuptools import find_packages, setup
 
 from io import open
 
@@ -14,7 +11,7 @@ setup(
     description="보카로 가사 위키에서 가사를 검색해주는 라이브러리",
     author="노토리",
     install_requires=requirements,
-    py_modules=["VocaroLyrics"],
+    packages=find_packages(),
     long_description="[보카로 가사 위키](http://vocaro.wikidot.com/)",
     url="https://github.com/NO-TORY/VocaroLyricsSearcher",
     long_description_content_type="text/markdown",
